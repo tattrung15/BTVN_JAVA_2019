6275cbe67aea551e0546aa599cb6567f9c71029c
Bùi Tất Trung <tattrung15@users.noreply.github.com>
2019-12-05 23:12:32 +0700

    a

--- BTVN_Buoi9/src/Bai1/RunMain.java
+++ BTVN_Buoi9/src/Bai1/RunMain.java
@@ -11,7 +11,25 @@ package Bai1;
  */
 public class RunMain {
     public static void main(String[] args) {
-        Cal cal = new Cal();
-        cal.HienThiHam(4, 2, 8);
+        HienThiHam(4, 2, 8);
+    }
+    
+    static int TinhTong(int a, int b, int c){
+        return a + b + c;
+    }
+    
+    static double TinhTheoCongThuc(int a, int b, int c){
+        return ((a / b)-c)/2;
+    }
+    
+    static String ChuyenThanhString(int a, int b, int c){
+        String s = String.valueOf(a) + String.valueOf(b) + String.valueOf(c);
+        return s;
+    }
+    
+    static void HienThiHam(int a, int b, int c){
+        System.out.println("Tinh Tong: " + TinhTong(a, b, c));
+        System.out.println("Tinh Theo Cong Thuc: " + TinhTheoCongThuc(a, b, c));
+        System.out.println("Chuyen Thanh String: " + ChuyenThanhString(a, b, c));
     }
 }
